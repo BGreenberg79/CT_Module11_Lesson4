fd83cdacc48ab8baedd393ffd3e96bb6f1890005
BGreenberg79 <greenbeb2@gmail.com>
2024-10-24 22:25:44 -0400

    2nd commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,11 @@
+// Task 2.3 Not Found Component
+
+function NotFound() {
+    return(
+        <div>
+            <h2>404- Not Found</h2>
+            <p>The page you are seeking does not exist.</p>
+            <p>Please return to the <Link to="/">homepage</Link></p>
+        </div>
+    )
+}
